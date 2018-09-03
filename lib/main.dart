138d74d1d776a01a54f3d62d1fedd0dcfc892245
Sam <zhao.shengjun@gmail.com>
2018-09-03 23:05:46 +1000

    add button

--- lib/main.dart
+++ lib/main.dart
@@ -11,10 +11,21 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: Text('EasyList'),
         ),
-        body: Card(
-          child: Column(
-            children: <Widget>[Image.asset('assets/food.jpg'), Text('Food Paradise')],
-          ),
+        body: Column(
+          children: <Widget>[
+            Container(
+              margin: EdgeInsets.all(10.0),
+              child: RaisedButton(
+                onPressed: () {},
+                child: Text('Add Product'),
+              ),
+            ),
+            Card(
+              child: Column(
+                children: <Widget>[Image.asset('assets/food.jpg'), Text('Food Paradise')],
+              ),
+            )
+          ],
         ),
       ),
     );
