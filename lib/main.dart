add61c5057986e3bef61b3578c1b2accd2d99e6a
Sam <zhao.shengjun@gmail.com>
2018-09-03 22:19:58 +1000

    basic scaffold

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,18 @@
+import 'package:flutter/material.dart';
 
+main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('EasyList'),
+        ),
+      ),
+    );
+  }
+}
