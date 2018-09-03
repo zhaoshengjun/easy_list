f31e47bac335e26962433fe592ca833c37b992af
Sam <zhao.shengjun@gmail.com>
2018-09-03 22:51:49 +1000

    add card and assets

--- lib/main.dart
+++ lib/main.dart
@@ -6,9 +6,15 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(primaryColor: Colors.deepOrange),
       home: Scaffold(
         appBar: AppBar(
           title: Text('EasyList'),
+        ),
+        body: Card(
+          child: Column(
+            children: <Widget>[Image.asset('assets/food.jpg'), Text('Food Paradise')],
+          ),
         ),
       ),
     );
