d93cd03a3732b0ef9f4f67a9abc096123de1afcb
Sam <zhao.shengjun@gmail.com>
2018-09-05 22:58:14 +1000

    play with color

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(primaryColor: Colors.deepOrange),
+      theme: ThemeData(brightness: Brightness.light, primaryColor: Colors.deepOrange, accentColor: Colors.deepPurple),
       home: Scaffold(
           appBar: AppBar(
             title: Text('EasyList'),
