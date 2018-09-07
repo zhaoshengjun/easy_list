15ef36b158074b32a3c18be036f3138a1557db55
Sam <zhao.shengjun@gmail.com>
2018-09-07 22:36:30 +1000

    use listview.builder to improve performance

--- lib/products.dart
+++ lib/products.dart
@@ -5,15 +5,19 @@ class Products extends StatelessWidget {
 
   Products(this.products);
 
+  Widget _buildProductItem(BuildContext context, int index) {
+    return Card(
+      child: Column(
+        children: <Widget>[Image.asset('assets/food.jpg'), Text(products[index])],
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return ListView(
-        children: products
-            .map((el) => Card(
-                  child: Column(
-                    children: <Widget>[Image.asset('assets/food.jpg'), Text(el)],
-                  ),
-                ))
-            .toList());
+    return ListView.builder(
+      itemBuilder: _buildProductItem,
+      itemCount: products.length,
+    );
   }
 }
