e79070c6ca11fe3c78a94cc511ac30e9fa0e3bbc
Sam <zhao.shengjun@gmail.com>
2018-09-07 23:08:02 +1000

    alternative way to do empty content

--- lib/products.dart
+++ lib/products.dart
@@ -15,13 +15,17 @@ class Products extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return products.length > 0
-        ? ListView.builder(
-            itemBuilder: _buildProductItem,
-            itemCount: products.length,
-          )
-        : Center(
-            child: Text('No product found. Please add some.'),
-          );
+    Widget productCard;
+    if (products.length > 0) {
+      productCard = ListView.builder(
+        itemBuilder: _buildProductItem,
+        itemCount: products.length,
+      );
+    } else {
+      productCard = Center(
+        child: Text('No product found. Please add some.'),
+      );
+    }
+    return productCard;
   }
 }
