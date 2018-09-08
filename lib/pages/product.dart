802a641599e3bf151cf4dee24131f5294cf807c1
Sam <zhao.shengjun@gmail.com>
2018-09-08 22:04:15 +1000

    add product page

---
+++ lib/pages/product.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ProductPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Product Details'),
+      ),
+      body: Center(
+        child: Text('On product page'),
+      ),
+    );
+  }
+}
