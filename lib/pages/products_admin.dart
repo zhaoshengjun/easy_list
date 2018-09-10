3db1ebf96d766674c83c8d58f0c2f78e3e4678d9
Sam <zhao.shengjun@gmail.com>
2018-09-10 23:25:49 +1000

    add tabs

--- lib/pages/products_admin.dart
+++ lib/pages/products_admin.dart
@@ -5,24 +5,36 @@ import './products.dart';
 class ProductsAdminPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      drawer: Drawer(
-        child: Column(
-          children: <Widget>[
-            AppBar(automaticallyImplyLeading: false, title: Text('Choose')),
-            ListTile(
-              title: Text('All Products'),
-              onTap: () {
-                Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => ProductsPage()));
-              },
-            )
-          ],
-        ),
-      ),
-      appBar: AppBar(
-        title: Text('Manage Products'),
-      ),
-      body: Center(child: Text('Manage your products')),
-    );
+    return DefaultTabController(
+        length: 2,
+        child: Scaffold(
+          drawer: Drawer(
+            child: Column(
+              children: <Widget>[
+                AppBar(automaticallyImplyLeading: false, title: Text('Choose')),
+                ListTile(
+                  title: Text('All Products'),
+                  onTap: () {
+                    Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => ProductsPage()));
+                  },
+                )
+              ],
+            ),
+          ),
+          appBar: AppBar(
+            title: Text('Manage Products'),
+            bottom: TabBar(
+              tabs: <Widget>[
+                Tab(
+                  text: 'Create Product',
+                ),
+                Tab(
+                  text: 'My Products',
+                )
+              ],
+            ),
+          ),
+          body: Center(child: Text('Manage your products')),
+        ));
   }
 }
