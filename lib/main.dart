f20a21f14ba084565c19cb52dd48274e7e9a771a
Sam <zhao.shengjun@gmail.com>
2018-09-11 22:43:46 +1000

    dynamic route

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import './pages/auth.dart';
 import './pages/products_admin.dart';
 import './pages/products.dart';
+import './pages/product.dart';
 
 main() => runApp(MyApp());
 
@@ -13,6 +14,18 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(brightness: Brightness.light, primaryColor: Colors.deepOrange, accentColor: Colors.deepPurple),
       // home: AuthPage(),
       routes: {'/': (BuildContext context) => ProductsPage(), 'admin': (BuildContext context) => ProductsAdminPage()},
+      onGenerateRoute: (RouteSettings settings) {
+        final List<String> pathElements = settings.name.split('/');
+        if (pathElements[0] != '') {
+          return null;
+        }
+
+        if (pathElements[1] == 'product') {
+          final int index = int.parse(pathElements[2]);
+          return MaterialPageRoute(builder: (BuildContext context) => ProductPage(products[index]));
+        }
+        return null;
+      },
     );
   }
 }
