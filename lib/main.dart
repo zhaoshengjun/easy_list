cbc67001c983af60bf47606d5f694b55ccba471a
Sam <zhao.shengjun@gmail.com>
2018-09-11 22:30:43 +1000

    special route for home page

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 import './pages/auth.dart';
 import './pages/products_admin.dart';
+import './pages/products.dart';
 
 main() => runApp(MyApp());
 
@@ -10,8 +11,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(brightness: Brightness.light, primaryColor: Colors.deepOrange, accentColor: Colors.deepPurple),
-      home: AuthPage(),
-      routes: {'admin': (BuildContext context) => ProductsAdminPage()},
+      // home: AuthPage(),
+      routes: {'/': (BuildContext context) => ProductsPage(), 'admin': (BuildContext context) => ProductsAdminPage()},
     );
   }
 }
