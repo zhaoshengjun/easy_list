f47c189e3b28953723ba3a56ccdd0fb573a19fa3
Sam <zhao.shengjun@gmail.com>
2018-09-11 22:55:47 +1000

    lift state up

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,41 @@
 import 'package:flutter/material.dart';
 
-import './pages/auth.dart';
+// import './pages/auth.dart';
 import './pages/products_admin.dart';
 import './pages/products.dart';
 import './pages/product.dart';
 
 main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => new _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  List<Map<String, String>> _products = [];
+
+  void _addProduct(Map product) {
+    setState(() {
+      _products.add(product);
+    });
+  }
+
+  void _deleteProduct(int index) {
+    setState(() {
+      _products.removeAt(index);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(brightness: Brightness.light, primaryColor: Colors.deepOrange, accentColor: Colors.deepPurple),
       // home: AuthPage(),
-      routes: {'/': (BuildContext context) => ProductsPage(), 'admin': (BuildContext context) => ProductsAdminPage()},
+      routes: {
+        '/': (BuildContext context) => ProductsPage(_products, _addProduct, _deleteProduct),
+        'admin': (BuildContext context) => ProductsAdminPage()
+      },
       onGenerateRoute: (RouteSettings settings) {
         final List<String> pathElements = settings.name.split('/');
         if (pathElements[0] != '') {
@@ -22,7 +44,7 @@ class MyApp extends StatelessWidget {
 
         if (pathElements[1] == 'product') {
           final int index = int.parse(pathElements[2]);
-          return MaterialPageRoute(builder: (BuildContext context) => ProductPage(products[index]));
+          return MaterialPageRoute(builder: (BuildContext context) => ProductPage(_products[index]));
         }
         return null;
       },
--- lib/product_manager.dart
+++ lib/product_manager.dart
@@ -3,46 +3,27 @@ import 'package:flutter/material.dart';
 import './products.dart';
 import 'product_control.dart';
 
-class ProductManager extends StatefulWidget {
-  final Map initProduct;
+class ProductManager extends StatelessWidget {
+  final List<Map<String, String>> products;
+  final Function addProduct;
+  final Function deleteProduct;
 
-  ProductManager({this.initProduct});
+  ProductManager(this.products, this.addProduct, this.deleteProduct);
 
-  @override
-  State<StatefulWidget> createState() {
-    return _ProductManagerState();
-  }
-}
-
-class _ProductManagerState extends State<ProductManager> {
-  List<Map> _products = [];
-
-  void _addProduct(Map product) {
-    setState(() {
-      _products.add(product);
-    });
-  }
-
-  void _deleteProduct(int index) {
-    setState(() {
-      _products.removeAt(index);
-    });
-  }
-
-  @override
-  void initState() {
-    if (widget.initProduct != null) {
-      _products.add(widget.initProduct);
-    }
-    super.initState();
-  }
+  // @override
+  // void initState() {
+  //   if (widget.initProduct != null) {
+  //     _products.add(widget.initProduct);
+  //   }
+  //   super.initState();
+  // }
 
   @override
   Widget build(BuildContext context) {
     return Column(
       children: <Widget>[
-        Container(margin: EdgeInsets.all(5.0), child: ProductControl(_addProduct)),
-        Expanded(child: ProductsPage(products: _products, deleteHandler: _deleteProduct))
+        Container(margin: EdgeInsets.all(5.0), child: ProductControl(addProduct)),
+        Expanded(child: ProductsPage(products: products, deleteHandler: deleteProduct))
       ],
     );
   }
