491e18a88d2daea8c53448d5fbd6408ff6098dae
Sam <zhao.shengjun@gmail.com>
2018-09-11 22:16:54 +1000

    tab navigation

--- lib/pages/product_list.dart
+++ lib/pages/product_list.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 class ProductListPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Text('Product list'),
+    );
   }
 }
