9a06f4d7db771e14fb541b001332537ce815d968
Sam <zhao.shengjun@gmail.com>
2018-09-13 22:43:04 +1000

    change widge to show all widgets

--- lib/pages/product_create.dart
+++ lib/pages/product_create.dart
@@ -16,7 +16,7 @@ class _ProductCreatePageState extends State<ProductCreatePage> {
   Widget build(BuildContext context) {
     return Container(
         margin: EdgeInsets.all(10.0),
-        child: Column(
+        child: ListView(
           children: <Widget>[
             TextField(
               decoration: InputDecoration(labelText: 'Product Title'),
