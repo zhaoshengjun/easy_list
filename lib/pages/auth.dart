571d960f51d8d1cec7ebba7473305f92d9bcc1dd
Sam <zhao.shengjun@gmail.com>
2018-09-19 22:06:00 +1000

    add widgets to auth page

--- lib/pages/auth.dart
+++ lib/pages/auth.dart
@@ -8,20 +8,42 @@ class AuthPage extends StatefulWidget {
 }
 
 class _AuthPageState extends State<AuthPage> {
+  String _email;
+  String _password;
+  bool _acceptTerms = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Login'),
       ),
-      body: Center(
-        child: RaisedButton(
-          child: Text('Login'),
-          onPressed: () {
-            Navigator.pushReplacementNamed(context, '/');
-          },
-        ),
-      ),
+      body: Container(
+          margin: EdgeInsets.all(10.0),
+          child: ListView(
+            children: <Widget>[
+              TextField(
+                decoration: InputDecoration(labelText: 'eMail'),
+                onChanged: (String value) {},
+              ),
+              TextField(
+                decoration: InputDecoration(labelText: 'Password'),
+                onChanged: (String value) {},
+              ),
+              SwitchListTile(
+                title: Text('Accept Terms'),
+              ),
+              SizedBox(
+                height: 10.0,
+              ),
+              RaisedButton(
+                child: Text('Login'),
+                onPressed: () {
+                  Navigator.pushReplacementNamed(context, '/');
+                },
+              ),
+            ],
+          )),
     );
   }
 }
